436df62dfae4fc8f9504f80790bc6215378d9f3d
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-02-01 10:42:58 +0100

    fronted user admin test

--- admin_views/user_list.php
+++ admin_views/user_list.php
@@ -5,6 +5,13 @@
     <title></title>
   </head>
   <body>
-
+    <h1>Utilisateurs</h1>
+    <div class="">
+      <span>nom</span>
+      <label for="admin">admin</label>
+      <input type="radio" id="admin" name="auth" value="0">
+      <label for="label">regular</label>
+      <input type="radio" id="regular" name="auth" value="1">
+    </div>
   </body>
 </html>
