0098cfb7acb83cc7cf6b31f918d08300811e51cd
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-02-06 16:35:31 +0100

    export tables

---
+++ akkad.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 01, 2018 at 04:43 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akkad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `citations`
+--
+
+CREATE TABLE `citations` (
+  `id` int(11) NOT NULL,
+  `citation` text NOT NULL,
+  `ln` varchar(4) NOT NULL,
+  `categorie` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `citations`
+--
+
+INSERT INTO `citations` (`id`, `citation`, `ln`, `categorie`) VALUES
+(1, 'Citation santé', 'fr', 'sante');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wings_user`
+--
+
+CREATE TABLE `wings_user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `auth` tinyint(4) NOT NULL DEFAULT '1' COMMENT '0:user admin :peut ajouter des utilisateurs, 1:user normal ,peut uniquement publier du contenu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `wings_user`
+--
+
+INSERT INTO `wings_user` (`id`, `name`, `password`, `auth`) VALUES
+(2, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `citations`
+--
+ALTER TABLE `citations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wings_user`
+--
+ALTER TABLE `wings_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `citations`
+--
+ALTER TABLE `citations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `wings_user`
+--
+ALTER TABLE `wings_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
