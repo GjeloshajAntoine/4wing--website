3d1808ae215505a0b0e5dcf3e91f93b518991c0e
Unknown <gjeloshaj.antoine@gmail.com>
2018-03-01 12:28:39 +0100

    Vue template variables

--- Views/tech_education.php
+++ Views/tech_education.php
@@ -1,10 +1,11 @@
 <?php require_once("../partials/header.php") ?>
+<include href="./Views/header_final.php" />
 
 <div class="container" id="TechEd">
     <div class="row header_bleu" >
       <div class="TechEducation col-md-8">
-        <span class="icon-monitor"></span><h1>Tech - Education</h1>
-        <span>Programmer le cercle vertueux &nbsp; de l'inclusion</span>
+        <span class="icon-monitor"></span><h1>Tech - Education {{@TitreH1}}</h1>
+        <span>Programmer le cercle vertueux &nbsp; de l'inclusion {{@SousTitreH1}}</span>
       </div>
     </div>
 
@@ -15,8 +16,8 @@
 		      <h2>Lire, écrire, compter et coder</h2>
 		      <p>4wings est consciente que la tech-education permet qu’un maximum de citoyens puissent saisir l’opportunité numérique, quel que soit leur âge, leur parcours ou leur situation. En Belgique, environ 30.000 emplois seront à pourvoir dans ce secteur d’ici 2020. La Fondation a la volonté de participer à l’émergence d’un écosystème digital plus inclusif au sein duquel lire, écrire, compter et coder sont des compétences accessibles à tous.</p>
 		    </div>
-       
-        <div class="TechSecond">     
+
+        <div class="TechSecond">
           <p> <span class="lettrine1">"</span> Le développement de la tech-education permet à des jeunes de se construire en apprenant le code et donc les technologies de demain.<span class="lettrine2"> " </span>
             </p>
           <h5>Matthieu Speeckaert, <br/> de 4WINGS</h5>
@@ -24,16 +25,16 @@
               <img src="../img/femme2.png" alt="femme">
             </div>
          </div>
-      
-		  </div>  
+
+		  </div>
 	    <div class="col-ms-3 TechSecondRight" >
 	      <h5>Un projet ?</h5>
 	      <a class="btn btn_success" href="soumission.php" target="blank">Nous contacter</a>
 	      <img src="../img/libelulle_transparente.png" alt="libellule">
 	    </div>
-	
-      
-    </div>  
+
+
+    </div>
   <!-- fin row-->
 </div>
 
@@ -49,7 +50,7 @@
           </div>
         </div>
   </div>
-</div>  
+</div>
 
 
 <div class="container" id="TechForth">
@@ -83,11 +84,11 @@
         </div>
     </div>
     <!-- fin projet-->
-    
+
 
   </div>
 </div>
- 
+
 
 
 <?php require_once("../partials/footer.php") ?>
