28e1fac381e2a26f38e0d32e98b4aed6560b9d5a
Unknown <gjeloshaj.antoine@gmail.com>
2018-03-01 13:09:41 +0100

    bootstrap link template cache

---
+++ tmp/gcr95nby72n4.2mqxto89hx4wg.php
@@ -0,0 +1,94 @@
+
+<?php echo $this->render('./Views/header_final.php',NULL,get_defined_vars(),0); ?>
+
+<div class="container" id="TechEd">
+    <div class="row header_bleu" >
+      <div class="TechEducation col-md-8">
+        <span class="icon-monitor"></span><h1>Tech - Education <?= $TitreH1 ?></h1>
+        <span>Programmer le cercle vertueux &nbsp; de l'inclusion <?= $SousTitreH1 ?></span>
+      </div>
+    </div>
+
+ <!-- Row -->
+    <div class="row">
+	  	<div class="col-sm-9">
+		    <div class="TechFirst">
+		      <h2>Lire, écrire, compter et coder</h2>
+		      <p>4wings est consciente que la tech-education permet qu’un maximum de citoyens puissent saisir l’opportunité numérique, quel que soit leur âge, leur parcours ou leur situation. En Belgique, environ 30.000 emplois seront à pourvoir dans ce secteur d’ici 2020. La Fondation a la volonté de participer à l’émergence d’un écosystème digital plus inclusif au sein duquel lire, écrire, compter et coder sont des compétences accessibles à tous.</p>
+		    </div>
+
+        <div class="TechSecond">
+          <p> <span class="lettrine1">"</span> Le développement de la tech-education permet à des jeunes de se construire en apprenant le code et donc les technologies de demain.<span class="lettrine2"> " </span>
+            </p>
+          <h5>Matthieu Speeckaert, <br/> de 4WINGS</h5>
+            <div class="TechSecond">
+              <img src="../img/femme2.png" alt="femme">
+            </div>
+         </div>
+
+		  </div>
+	    <div class="col-ms-3 TechSecondRight" >
+	      <h5>Un projet ?</h5>
+	      <a class="btn btn_success" href="soumission.php" target="blank">Nous contacter</a>
+	      <img src="../img/libelulle_transparente.png" alt="libellule">
+	    </div>
+
+
+    </div>
+  <!-- fin row-->
+</div>
+
+</div>
+
+
+<div class="TechThird">
+  <div class="container">
+         <div class="row" >
+          <div class="col-sm-1"> <span class="icon-monitor"> </span></div>
+          <div class="TechEducation col-sm-11">
+            <h3>Les projets Tech - Education</h3>
+          </div>
+        </div>
+  </div>
+</div>
+
+
+<div class="container" id="TechForth">
+  <div class="row">
+    <!-- projet-->
+     <div class="col-sm-12 col-md-6">
+        <div class="TechForth">
+          <a href="projet_tech.php"><img src="../img/projets/SmallLogo.png" alt="logo becode"></a>
+          <div class="rubrique">
+            <h4> <a href="projet_tech.php">Becode</a> </h4>
+            <p>BeCode est une start-up à impact social
+                élevé qui, inspirée par le modèle français
+                de Simplon, vise à développer le talent de
+                jeunes infra-qualifiés pour leur permettre
+                de devenir les développeurs de demain.</p>
+          </div>
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-6">
+        <div class="TechForth">
+          <a href="projet_tech.php"><img src="../img/projets/SmallLogo.png" alt="logo becode"></a>
+          <div class="rubrique">
+            <h4> <a href="projet_tech.php">Becode</a> </h4>
+            <p>BeCode est une start-up à impact social
+                élevé qui, inspirée par le modèle français
+                de Simplon, vise à développer le talent de
+                jeunes infra-qualifiés pour leur permettre
+                de devenir les développeurs de demain.</p>
+          </div>
+        </div>
+    </div>
+    <!-- fin projet-->
+
+
+  </div>
+</div>
+
+
+
+
