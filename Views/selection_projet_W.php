9c071581e628977ce583a6267e0616caf719ecfb
weichuan.tran <weichuan.tran@gmail.com>
2018-03-02 16:37:24 +0100

    ajout formulaire et selection projet W newfichiers

--- Views/selection_projet_W.php
+++ Views/selection_projet_W.php
@@ -1,4 +1,30 @@
-<div class="container selection">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <meta charset="utf-8">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../stylesheets/jquery.jscrollpane.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="../stylesheets/style_carousel.css" />
+    <link rel="stylesheet" type="text/css" href="../stylesheets/jquery.jscrollpane.css" media="all" />
+    <link rel="stylesheet" href="../stylesheets/footer.css">
+    <link rel="stylesheet" href="../stylesheets/icomoon.css">
+    <link rel="stylesheet" href="../stylesheets/nav.css">
+    <link rel="stylesheet" href="../stylesheets/mobile.css">
+    <link rel="stylesheet" href="../stylesheets/mission.css">
+  <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>mission</title>
+  <body>
+    <?php require_once("../partials/header.php") ?>
+<div class="container Selection">
   <div class="row SelectionTitre">
     <h1>Sélection des projets</h1>
     <h2>Que pouvons nous faire pour vous ?</h2>
@@ -24,7 +50,8 @@
   </div>
   <div class="candidature">
     <h3>Candidatures Spontanées.</h3>
-    <p>Si votre projet répond aux critères ci-dessus, envoyez-nous une description succincte en utilisant ce <a href="?page=formulaire">formulaire</a>.
+    <p>Si votre projet répond aux critères ci-dessus, envoyez-nous une description succincte en utilisant ce <a href="formulaireSelection.php">formulaire</a>.
     </p>
   </div>
 </div>
+<?php include("../partials/footer.php") ?>
