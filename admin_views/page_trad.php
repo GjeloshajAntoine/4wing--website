bd8c3e1a335b3808d949ed193a2d52f0d0a6d00d
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-03-12 14:53:26 +0100

    wisyig pages trad

--- admin_views/page_trad.php
+++ admin_views/page_trad.php
@@ -5,10 +5,14 @@
     <title></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/master.css">
-    <script src="/4wing-website/wysihtml.min.js"></script>
 
+    <script src="/4wing-website/wysihtml.min.js"></script>
+    <!-- <script src="https://cdn.rawgit.com/Voog/wysihtml/master/dist/wysihtml.js" charset="utf-8"></script>
+    <script src="https://cdn.rawgit.com/Voog/wysihtml/master/dist/wysihtml.all-commands.js" charset="utf-8"></script> -->
     <script src="/4wing-website/wysihtml-toolbar.min.js"></script>
     <script src="/4wing-website/advanced_and_extended.js"></script>
+    <!-- <script src="https://cdn.rawgit.com/Voog/wysihtml/master/dist/wysihtml.toolbar.js" charset="utf-8"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/Voog/wysihtml/master/parser_rules/advanced_unwrap.js">  </script> -->
 
   </head>
   <body class="container">
@@ -24,19 +28,33 @@
     </ul>
     <div class="container">
     <form class="" action="http://localhost/4wing-website/admin/page_trad_changes/{{@pagename}}/{{@lg}}" method="post">
+
       <repeat group="{{ @all_trad }}" value="{{ @trad }}" counter="{{ @ctr }}" >
           <!-- <input type="text" value="{{ @trad.string_origin }}" disabled>
           <input type="text" value="{{ @trad.string_trad}}" name="{{ @trad.id }}" > -->
           <label for="{{ @ctr }}" >{{ @trad.string_origin }}</label>
-
-
           <div class="panel panel-default">
             <div class="panel-heading">
               <div id="toolbar-{{@ctr}}">
-                <a data-wysihtml5-command="bold">bold</a>
-                <a data-wysihtml5-command="italic">italic</a>
+                <a data-wysihtml5-command="bold">
+                  <span class="glyphicon glyphicon-bold"></span>
+                  bold
+                </a>
+                <a data-wysihtml5-command="italic">
+                  <span class="glyphicon glyphicon-italic"></span>
+                  italic
+                </a>
                 <a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1">H1</a>
                 <a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="p">P</a>
+                <a data-wysihtml5-command="createLink">insert link</a>
+                <div data-wysihtml5-dialog="createLink" style="display:none;">
+                  <label>
+                    Link:
+                    <input data-wysihtml5-dialog-field="href" value="http://">
+                  </label>
+                  <a data-wysihtml5-dialog-action="save">OK</a>
+                  <a data-wysihtml5-dialog-action="cancel">Cancel</a>
+                </div>
               </div>
             </div>
             <div class="panel-body">
@@ -46,16 +64,18 @@
           </div>
           <br/>
       </repeat>
+
       <input type="submit" class="btn btn-default">
     </form>
     <div id="nbrOfEditor" data-total="{{ @ctr }}"></div>
     </div>
     <script>
-      var editor = new wysihtml5.Editor('1', {toolbar: 'toolbar-1',parserRules:  wysihtml5ParserRules});
+      //var editor = new wysihtml5.Editor('1', {toolbar: 'toolbar-1',parserRules:  wysihtml5ParserRules});
       var editArray=[];
       var nrbOfEditor=document.getElementById('nbrOfEditor') ;
       console.log(nbrOfEditor.dataset.total);
-      for (var i = 1; i < nbrOfEditor.dataset.total; i++) {
+      //wysihtml5ParserRules.tags.a.check_attributes.href="url";
+      for (var i = 1; i <= nbrOfEditor.dataset.total; i++) {
         editArray.push( new wysihtml5.Editor(''+i, {toolbar: 'toolbar-'+i,parserRules:  wysihtml5ParserRules}) );
       }
     </script>
