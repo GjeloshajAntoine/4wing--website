7c5fd17808174f4a994cd6b18c8770c34fc6d309
Unknown <gjeloshaj.antoine@gmail.com>
2018-03-16 22:00:07 +0100

    Projet indo corrections

--- routes/projet_routes.php
+++ routes/projet_routes.php
@@ -3,6 +3,7 @@
 $f3->route('GET /admin/projet/list',function ($f3,$params) {
   is_connected_with(false,$f3,function($f3){
     $f3->set('all_projects',Project::get_all_projects());
+    $f3->set('active','projets');
     echo Template::instance()->render('admin_views/projet_list.php');
   });
 });
@@ -14,14 +15,19 @@ $f3->route('GET /admin/projet/@id',function ($f3,$params) {
 });
 $f3->route('GET /admin/projet/@id/info',function ($f3,$params) {//info principales nom,catégories
   is_connected_with(false,$f3,function($f3){
-    $f3->set('project',Project::get_info($f3->PARAMS['id']));
+    $projet=Project::get_info($f3->PARAMS['id']);
+    $f3->set('project',$projet);
     $f3->set('id',$f3->PARAMS['id']);
+    $f3->set('breadcrumb',['projets'=>'/admin/projet/list',]);
+    $f3->set('active',$projet["title"]);
     echo Template::instance()->render('admin_views/projet_info.php');
   });
 });
 $f3->route('POST /admin/projet/@id/info/editdata',function ($f3,$params) {
   is_connected_with(false,$f3,function($f3){
-    Project::set_info();
+    var_dump($_POST);
+    Project::set_info($_POST['id'],$_POST['title'],$_POST['cat']);
+    $f3->set('id',$f3->PARAMS['id']);
     $f3->reroute('/admin/projet/@id/info');
   });
 });
@@ -30,6 +36,12 @@ $f3->route('GET /admin/projet/@id/trad/@lg',function ($f3,$params) {
     $f3->set('all_trad',Project::get_trad($f3->PARAMS['id'],$f3->PARAMS['lg']));
     $f3->set('id',$f3->PARAMS['id']);
     $f3->set('lg',$f3->PARAMS['lg']);
+    $projet=Project::get_info($f3->PARAMS['id']);
+    $f3->set('project',$projet);
+    $f3->set('id',$f3->PARAMS['id']);
+    $f3->set('breadcrumb',['projets'=>'/admin/projet/list',]);
+    $f3->set('active',$projet["title"]);
+
     echo Template::instance()->render('admin_views/projet_trad.php');
   });
 });
@@ -43,6 +55,9 @@ $f3->route('POST /admin/projet/@id/trad/@lg/editdata',function ($f3,$params) {
 
 $f3->route('GET /admin/projet/create',function ($f3,$params) {
   is_connected_with(false,$f3,function($f3){
+    $f3->set('breadcrumb',['projets'=>'/admin/projet/list',]);
+    $f3->set('active','nouveau');
+
     echo Template::instance()->render('admin_views/project_new.php');
   });
 });
@@ -58,20 +73,27 @@ $f3->route('POST /admin/projet/create_data',function ($f3,$params) {
 $f3->route('GET /admin/projet/@id/images/list',function ($f3,$params) {
   is_connected_with(false,$f3,function($f3){
     $f3->set("all_images",Project::get_image_from_id($f3->PARAMS['id']));
-      $f3->set('id',$f3->PARAMS['id']);
+    $f3->set('id',$f3->PARAMS['id']);
+    $projet=Project::get_info($f3->PARAMS['id']);
+    $f3->set('project',$projet);
+    $f3->set('id',$f3->PARAMS['id']);
+    $f3->set('breadcrumb',['projets'=>'/admin/projet/list',]);
+    $f3->set('active',$projet["title"]);
+
     echo Template::instance()->render('admin_views/projet_image_updload.php');
   });
 });
 $f3->route('POST /admin/projet/@id/image/add',function ($f3,$params) {
   is_connected_with(false,$f3,function($f3){
      Project::add_image_to_id($f3->PARAMS['id'],$_FILES['file']);
-    echo Template::instance()->render('admin_views/');
+     $f3->reroute('/admin/projet/@id/images/list');
   });
 });
-$f3->route('POST /admin/projet/@id/image/delete',function ($f3,$params) {
+$f3->route('GET /admin/projet/@id/image/delete',function ($f3,$params) {
   is_connected_with(false,$f3,function($f3){
-     Project::add_image_to_id($f3->PARAMS['id'],$_FILES['file']);
-    echo Template::instance()->render('admin_views/');
+     Project::delete_image_to_id($f3->PARAMS['id'],$_FILES['file']);
+     $f3->set('id',$f3->PARAMS['id']);
+     $f3->reroute('/admin/projet/@id/images/list');
   });
 });
 $f3->route('POST /admin/projet/@id/logo/change',function ($f3,$params) {
