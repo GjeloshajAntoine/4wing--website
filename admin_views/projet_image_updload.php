a10fbafe53420e3f0b20f364a8c436c9756055e7
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-03-19 12:07:15 +0100

    route ajax

--- admin_views/projet_image_updload.php
+++ admin_views/projet_image_updload.php
@@ -12,7 +12,7 @@
       <include href="admin_views/breadcrumb.php"/>
       <include href="admin_views/project_tabs.php" with="tabname=images" />
 
-      <form class="" action="../../../../admin/projet/{{@id}}/image/add" method="post" enctype="multipart/form-data">
+      <form class="" action="{{@subrootpath}}/admin/projet/{{@id}}/image/add" method="post" enctype="multipart/form-data">
         <input id="file" type="file" name="file" value="envoyer image" />
         <input type="hidden" id="ProjectId" value="{{project.id}}">
         <input type="submit" name="" value="ajouter">
@@ -21,9 +21,9 @@
       <ul class="list-group">
         <repeat group="{{ @all_images }}" value="{{ @image }}">
           <li class="list-group-item">
-            <img width="50" height="50" src="../../../../projects_images/{{@image.image_file_name}}" alt="">
+            <img width="50" height="50" src="{{@subrootpath}}/projects_images/{{@image.image_file_name}}" alt="">
             <!-- <button style="float:right;" type="button" class="btn btn-danger">supprimer</button> -->
-            <a style="float:right;" class="btn btn-danger" href="{{@subrootpath}}/admin/projet/{{@image.id}}/image/delete">supprimer</a>
+            <a style="float:right;" class="btn btn-danger" href="{{@subrootpath}}/admin/projet/{{@project.id}}/image/{{@image.id}}/delete">supprimer</a>
           </li>
         </repeat>
       </ul>
