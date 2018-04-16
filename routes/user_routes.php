46306ee9fc266657af6615a9341a989d22e39b3b
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-04-16 09:40:13 +0200

    upload

--- routes/user_routes.php
+++ routes/user_routes.php
@@ -20,12 +20,19 @@ $f3->route('GET /admin/user/new',function ($f3,$params) {
 });
 $f3->route('POST /admin/user/change_auth',function ($f3,$params) {
   is_connected_with(true,$f3,function($f3){
-
+    echo json_encode(change_auth($_POST['id'],$_POST['auth']));
   });
 });
 $f3->route('POST /admin/user/recreate_password',function ($f3,$params) {
   is_connected_with(true,$f3,function($f3){
-
+    header("Content-type:application/json");
+    echo json_encode(resetPassword($_POST['id']));
+  });
+});
+$f3->route('POST /admin/user/create_user',function ($f3,$params) {
+  is_connected_with(true,$f3,function($f3){
+    header("Content-type:application/json");
+    echo json_encode(create_user($_POST['email'],$_POST['auth']));
   });
 });
 
